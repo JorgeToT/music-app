4b818befabf135d7dcfe9b954c1745dd52ccb0aa
Jorge Aguilar <jorge.agvel@gmail.com>
2022-06-17 17:46:45 -0500

    Create new user on our databse with post method

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -2,17 +2,18 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import cookie from "cookie";
 import prisma from "../../lib/prisma";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const salt = bcrypt.genSaltSync();
   const { email, password } = req.body;
-
-  let user;
+  console.log(email, password);
+  let user: any;
 
   try {
     user = await prisma.user.create({
       data: {
-        email,
+        email: email,
         password: bcrypt.hashSync(password, salt),
       },
     });
