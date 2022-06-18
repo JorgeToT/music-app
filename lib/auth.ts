dc7349514bcb6152f43e44697fb5cacc186fe679
Jorge Aguilar <jorge.agvel@gmail.com>
2022-06-18 04:51:39 -0500

    Create functions for POST or GET methods depending the view (signin or signup) and authorization for the user

---
+++ lib/auth.ts
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "./prisma";
+
+export const validateRoute = (handler: any) => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const token = req.cookies.TRAX_ACCES_TOKEN;
+
+    if (token) {
+      let user: any;
+
+      try {
+        const { id } = jwt.verify(token, "Hello");
+        user = await prisma.user.findUnique({ where: { id } });
+
+        if (!user) {
+          throw new Error("User not found");
+        }
+      } catch (error) {
+        res.status(401);
+        res.json({ error: "Not autorized" });
+        return;
+      }
+      return handler(req, res, user);
+    }
+    res.status(401);
+    res.json({ error: "Not autorized" });
+  };
+};
