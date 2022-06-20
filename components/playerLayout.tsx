3ec7bba19ae29e3d3ad8c0cf35b3486ea266ff0d
Jorge Aguilar <jorge.agvel@gmail.com>
2022-06-20 01:16:07 -0500

    check styles

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -5,15 +5,21 @@ const PlayerLayout = ({ children }) => {
   return (
     <Box width="100vw" height="100vh">
       <Box position="absolute" top="0" width="250px" left="0">
-        <Sidebar />
-      </Box>
-      <Box marginLeft="250px" marginBottom={"100px"}>
         <Box height={"calc(100vh - 100px)"}>
-          {children}
+          <Sidebar />
         </Box>
       </Box>
-      <Box position={"absolute"} left={"0"} bottom={"0"}>
-        player
+      <Box marginLeft="250px" marginBottom={"100px"}>
+        <Box height={"calc(100vh - 100px)"}>{children}</Box>
+      </Box>
+      <Box
+        position={"absolute"}
+        left={"0"}
+        bottom={"0"}
+        height={"100px"}
+        width="100vw"
+      >
+        Player
       </Box>
     </Box>
   );
