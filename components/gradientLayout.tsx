166bc530e32539140990a371d2a594066d8aa372
Jorge Aguilar <jorge.agvel@gmail.com>
2022-06-19 21:04:11 -0500

    Create main layout

---
+++ components/gradientLayout.tsx
@@ -0,0 +1,35 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
+
+const GradientLayout = (props) => {
+  const color = props.color || "blue";
+  return (
+    <Box
+      height={"100%"}
+      overflowY="auto"
+      bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.95) 75%)`}
+    >
+      <Flex bg={`${color}.600`} padding="40px" align="end">
+        <Box padding="20px">
+          <Image
+            boxSize="160px"
+            boxShadow={"2xl"}
+            src={props.image}
+            borderRadius={props.roundImage ? "100%" : "3px"}
+          />
+        </Box>
+        <Box padding={"20px"} color={"white"}>
+          <Text fontSize="x-small" fontWeight="bold" casing={"uppercase"}>
+            {props.subtitle}
+          </Text>
+          <Text fontSize={"6xl"} height={"100%"}>{props.title}</Text>
+          <Text fontSize={"x-small"}>
+            {props.description}
+          </Text>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default GradientLayout;
