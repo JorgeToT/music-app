05c5437525ad288e8e2809e57b67cc452a6fcf13
Jorge Aguilar <jorge.agvel@gmail.com>
2022-06-20 01:21:27 -0500

    Create page for playlist by id

---
+++ pages/playlist/[id].tsx
@@ -0,0 +1,51 @@
+import GradientLayout from "../../components/gradientLayout";
+import { validateRoute, validateToken } from "../../lib/auth";
+import prisma from "../../lib/prisma";
+
+const getBGColor = (index) => {
+  const colors = ["blue", "green", "orange", "purple", "red", "teal", "yellow"];
+  return colors[index % colors.length];
+};
+
+const Playlist = ({ playlist }) => {
+  return (
+    <GradientLayout
+      color={getBGColor(playlist.id)}
+      roundImage={false}
+      title={playlist.name}
+      subtitle={"playlist"}
+      description={playlist.songs.length + " tracks"}
+      image={"https://picsum.photos/200?random=" + playlist.id}
+    ></GradientLayout>
+  );
+};
+
+export const getServerSideProps = async ({ query, req }) => {
+  const { id } = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
+  const [playlist] = await prisma.playlist.findMany({
+    where: {
+      id: +query.id,
+      userId: id,
+    },
+    include: {
+      songs: {
+        include: {
+          artist: {
+            select: {
+              name: true,
+              id: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  return {
+    props: {
+      playlist,
+    },
+  };
+};
+
+export default Playlist;
