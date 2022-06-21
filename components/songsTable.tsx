0a140651df1ccd0e182b5c679a8fcafbbb86f845
Jorge Aguilar <jorge.agvel@gmail.com>
2022-06-20 20:58:43 -0500

    Functions to select the song and appear on player layout

--- components/songsTable.tsx
+++ components/songsTable.tsx
@@ -3,8 +3,17 @@ import { Table, Thead, Td, Tr, Tbody, Th, IconButton } from "@chakra-ui/react";
 import { BsFillPlayFill } from "react-icons/bs";
 import { AiOutlineClockCircle } from "react-icons/ai";
 import { formatDate, formaTime } from "../lib/formatters";
+import { useStoreActions } from "easy-peasy";
 
 const SongTable = ({ songs }) => {
+  const playSongs = useStoreActions((store: any) => store.changeActiveSongs);
+  const setActiveSong = useStoreActions((store: any) => store.changeActiveSong);
+
+  const handlePlay = (activeSong?) => {
+    setActiveSong(activeSong || songs[0]);
+    playSongs(songs);
+  };
+
   return (
     <Box bg="transparent" color={"white"}>
       <Box padding={"10px"} marginBottom="30px">
@@ -14,6 +23,7 @@ const SongTable = ({ songs }) => {
           colorScheme="green"
           size="lg"
           isRound
+          onClick={() => handlePlay()}
         />
       </Box>
       <Table variant={"unstyled"}>
@@ -38,6 +48,7 @@ const SongTable = ({ songs }) => {
               }}
               key={song.id}
               cursor="pointer"
+              onClick={() => handlePlay(song)}
             >
               <Td>{i + 1}</Td>
               <Td>{song.name}</Td>
