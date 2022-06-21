0e0c86d275c675cd3ec36c5f840ff1ff24494c8b
Jorge Aguilar <jorge.agvel@gmail.com>
2022-06-20 23:54:10 -0500

    typescript and eslint errors in build

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   typescript: {
-    ignoreDevErrors: true,
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
